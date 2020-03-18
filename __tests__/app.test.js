f6f1a348abbb6715188ed71dec7690775775f6bb
janelle mellor <janellemellor@gmail.com>
2020-03-17 17:08:11 -0700

    update delete and patch tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -125,6 +125,11 @@ describe('app routes', () => {
         'put dough on cookie sheet',
         'bake for 10 minutes'
       ],
+      ingredients: [{
+        name: 'sugar',
+        amount: 2,
+        measurement: 'teaspoon'
+      }]
     });
 
     return request(app)
@@ -140,6 +145,12 @@ describe('app routes', () => {
             'put dough on cookie sheet',
             'bake for 10 minutes'
           ],
+          ingredients: [{
+            _id: expect.any(String),
+            name: 'sugar',
+            amount: 2,
+            measurement: 'teaspoon'
+          }],
           __v: 0
         });
       });
@@ -154,6 +165,11 @@ describe('app routes', () => {
           'put dough on cookie sheet',
           'bake for 10 minutes'
         ], 
+        ingredients: [{
+          name: 'sugar',
+          amount: 2,
+          measurement: 'teaspoon'
+        }]
       });
 
     return request(app)
@@ -168,6 +184,12 @@ describe('app routes', () => {
             'put dough on cookie sheet',
             'bake for 10 minutes'
           ],
+          ingredients: [{
+            _id: expect.any(String),
+            name: 'sugar',
+            amount: 2,
+            measurement: 'teaspoon'
+          }],
           __v: 0
         });
       });
