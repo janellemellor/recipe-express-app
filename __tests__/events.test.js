271d417a400f158fb5079e079978c1d28ce7ea4e
janelle mellor <janellemellor@gmail.com>
2020-03-20 18:06:22 -0700

    add failing test for create an event

--- __tests__/events.test.js
+++ __tests__/events.test.js
@@ -1,179 +1,230 @@
-// require('dotenv').config();
+require('dotenv').config();
 
-// const request = require('supertest');
-// const app = require('../lib/app');
-// const connect = require('../lib/utils/connect');
-// const mongoose = require('mongoose');
-// const Recipe = require('../lib/models/Recipe');
-// const Event = require('../lib/models/Event');
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+const Recipe = require('../lib/models/Recipe');
+const Event = require('../lib/models/Event');
 
-// describe('app routes', () => {
-//   beforeAll(() => {
-//     connect();
-//   });
+describe('event routes', () => {
+  beforeAll(() => {
+    connect();
+  });
 
-//   beforeEach(() => {
-//     return mongoose.connection.dropDatabase();
-//   });
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
 
-//   afterAll(() => {
-//     return mongoose.connection.close();
-//   });
+  let recipe;
+  let event;
+  beforeEach(async() => {
+    recipe = await Recipe.create(
+      { name: 'cookies', 
+        directions: [
+          'preheat oven to 375',
+          'mix ingredients',
+          'put dough on cookie sheet',
+          'bake for 10 minutes'
+        ], 
+        ingredients: [{
+          name: 'sugar',
+          amount: 2,
+          measurement: 'teaspoon'
+        }]
+      });
+  
+    event = await Event.create([
+      { recipeId: recipe._id,
+        dateOfEvent: new Date(),
+        notes: 'best.cookie.ever',
+        rating: 5 }
+    ]);
+  });
 
-//   it('creates an event', () => {
-//     return request(app)
-//       .post('/api/v1/events')
-//       .send({
-//         recipeId:
-//         dateOfEvent:
-//         notes:
-//         rating:
-//       })
-//       .then(res => {
-//         expect(res.body).toEqual({
-          
-//           __v: 0
-//         });
-//       });
-//   });
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
 
-//   it('gets all events', async() => {
-//     const events = await Recipe.create([
-//       { name: 'cookies', directions: [] },
-//       { name: 'cake', directions: [] },
-//       { name: 'pie', directions: [] }
-//     ]);
+  it('creates an event', () => {
+    return request(app)
+      .post('/api/v1/events')
+      .send({
+        recipeId: recipe._id,
+        dateOfEvent: Date.now(),
+        notes: 'best.cookie.ever',
+        rating: 5 
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          recipeId: recipe._id.toString(),
+          dateOfEvent: expect.any(String),
+          notes: 'best.cookie.ever',
+          rating: 5, 
+          __v: 0
+        });
+      });
+  });
 
-//     return request(app)
-//       .get('/api/v1/events')
-//       .then(res => {
-//         events.forEach(event => {
-//           expect(res.body).toContainEqual({
-//             _id: event._id.toString(),
-//             name: event.name
-//           });
-//         });
-//       });
-//   });
+  
+  it('gets all recipes', async() => {
+    const recipes = await Recipe.create([
+      { name: 'cookies', directions: [] },
+      { name: 'cake', directions: [] },
+      { name: 'pie', directions: [] }
+    ]);
 
-//   it('gets a specific event', async() => {
-//     const event = await Recipe.create(
-//       { name: 'cookies', 
-//         directions: [
-//           'preheat oven to 375',
-//           'mix ingredients',
-//           'put dough on cookie sheet',
-//           'bake for 10 minutes'
-//         ], 
-//         ingredients: [{
-//           name: 'sugar',
-//           amount: 2,
-//           measurement: 'teaspoon'
-//         }]
-//       });
+    return request(app)
+      .get('/api/v1/recipes')
+      .then(res => {
+        recipes.forEach(recipe => {
+          expect(res.body).toContainEqual({
+            _id: recipe._id.toString(),
+            name: recipe.name
+          });
+        });
+      });
+  });
 
-//     return request(app)
-//       .get(`/api/v1/events/${event._id}`)
-//       .then(res => {
-//         expect(res.body).toEqual({
-//           _id: expect.any(String),
-//           name: 'cookies',
-//           directions: [
-//             'preheat oven to 375',
-//             'mix ingredients',
-//             'put dough on cookie sheet',
-//             'bake for 10 minutes'
-//           ],
-//           ingredients: [{
-//             _id: expect.any(String),
-//             name: 'sugar',
-//             amount: 2,
-//             measurement: 'teaspoon'
-//           }],
-//           __v: 0
-//         });
-//       });
-//   });
+  it('gets a specific recipe and all events associated', async() => {
+    const recipe = await Recipe.create(
+      { name: 'cookies', 
+        directions: [
+          'preheat oven to 375',
+          'mix ingredients',
+          'put dough on cookie sheet',
+          'bake for 10 minutes'
+        ], 
+        ingredients: [{
+          name: 'sugar',
+          amount: 2,
+          measurement: 'teaspoon'
+        }]
+      });
 
+    await Event.create([
+      { recipeId: recipe.id,
+        dateOfEvent: Date.now(),
+        notes: 'best.cookie.ever',
+        rating: 5 },
+      { recipeId: recipe.id,
+        dateOfEvent: Date.now(),
+        notes: 'never make again',
+        rating: 1 },
+      { recipeId: recipe.id,
+        dateOfEvent: Date.now(),
+        notes: 'pretty ok cookie',
+        rating: 3 },
+    ]);
 
-//   it('updates a event by id', async() => {
-//     const event = await Recipe.create({
-//       name: 'cookies',
-//       directions: [
-//         'preheat oven to 375',
-//         'mix ingredients',
-//         'put dough on cookie sheet',
-//         'bake for 10 minutes'
-//       ],
-//       ingredients: [{
-//         name: 'sugar',
-//         amount: 2,
-//         measurement: 'teaspoon'
-//       }]
-//     });
-
-//     return request(app)
-//       .patch(`/api/v1/events/${event._id}`)
-//       .send({ name: 'good cookies' })
-//       .then(res => {
-//         expect(res.body).toEqual({
-//           _id: expect.any(String),
-//           name: 'good cookies',
-//           directions: [
-//             'preheat oven to 375',
-//             'mix ingredients',
-//             'put dough on cookie sheet',
-//             'bake for 10 minutes'
-//           ],
-//           ingredients: [{
-//             _id: expect.any(String),
-//             name: 'sugar',
-//             amount: 2,
-//             measurement: 'teaspoon'
-//           }],
-//           __v: 0
-//         });
-//       });
-//   });
-
-//   it('deletes a specific event', async() => {
-//     const event = await Recipe.create(
-//       { name: 'cookies', 
-//         directions: [
-//           'preheat oven to 375',
-//           'mix ingredients',
-//           'put dough on cookie sheet',
-//           'bake for 10 minutes'
-//         ], 
-//         ingredients: [{
-//           name: 'sugar',
-//           amount: 2,
-//           measurement: 'teaspoon'
-//         }]
-//       });
-
-//     return request(app)
-//       .get(`/api/v1/events/${event._id}`)
-//       .then(res => {
-//         expect(res.body).toEqual({
-//           _id: expect.any(String),
-//           name: 'cookies',
-//           directions: [
-//             'preheat oven to 375',
-//             'mix ingredients',
-//             'put dough on cookie sheet',
-//             'bake for 10 minutes'
-//           ],
-//           ingredients: [{
-//             _id: expect.any(String),
-//             name: 'sugar',
-//             amount: 2,
-//             measurement: 'teaspoon'
-//           }],
-//           __v: 0
-//         });
-//       });
-//   });
+    return request(app)
+      .get(`/api/v1/recipes/${recipe._id}`)
+      .then(res => {
+        expect(res.body.events).toHaveLength(3);
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'cookies',
+          directions: [
+            'preheat oven to 375',
+            'mix ingredients',
+            'put dough on cookie sheet',
+            'bake for 10 minutes'
+          ],
+          ingredients: [{
+            _id: expect.any(String),
+            name: 'sugar',
+            amount: 2,
+            measurement: 'teaspoon'
+          }],
+          events: expect.any(Array),
+          __v: 0
+        });
+      });
+  });
 
 
-// });
+  it('updates a recipe by id', async() => {
+    const recipe = await Recipe.create({
+      name: 'cookies',
+      directions: [
+        'preheat oven to 375',
+        'mix ingredients',
+        'put dough on cookie sheet',
+        'bake for 10 minutes'
+      ],
+      ingredients: [{
+        name: 'sugar',
+        amount: 2,
+        measurement: 'teaspoon'
+      }]
+    });
+
+    return request(app)
+      .patch(`/api/v1/recipes/${recipe._id}`)
+      .send({ name: 'good cookies' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'good cookies',
+          directions: [
+            'preheat oven to 375',
+            'mix ingredients',
+            'put dough on cookie sheet',
+            'bake for 10 minutes'
+          ],
+          ingredients: [{
+            _id: expect.any(String),
+            name: 'sugar',
+            amount: 2,
+            measurement: 'teaspoon'
+          }],
+          __v: 0
+        });
+      });
+  });
+
+  it('deletes a specific recipe and all associated events', async() => {
+    const recipe = await Recipe.create(
+      { name: 'cookies', 
+        directions: [
+          'preheat oven to 375',
+          'mix ingredients',
+          'put dough on cookie sheet',
+          'bake for 10 minutes'
+        ], 
+        ingredients: [{
+          name: 'sugar',
+          amount: 2,
+          measurement: 'teaspoon'
+        }]
+      });
+
+    return request(app)
+      .delete(`/api/v1/recipes/${recipe._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'cookies',
+          directions: [
+            'preheat oven to 375',
+            'mix ingredients',
+            'put dough on cookie sheet',
+            'bake for 10 minutes'
+          ],
+          ingredients: [{
+            _id: expect.any(String),
+            name: 'sugar',
+            amount: 2,
+            measurement: 'teaspoon'
+          }],
+          __v: 0
+        });
+
+        return Event.find();
+      })
+      .then(events => {
+        expect(events).toHaveLength(0);
+      });
+  });
+});
